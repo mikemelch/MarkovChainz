378eeeb2f12be393ef2a25800f8902597ca04106
Mike Melchione <michaelmelchione@gmail.com>
2016-01-05 18:35:58 -0500

    Updated Markov class with new DenseBag type

--- src/markov/Markov.java
+++ src/markov/Markov.java
@@ -4,17 +4,21 @@ import java.util.HashMap;
 import java.util.Vector;
 
 public class Markov {
-	private HashMap<String, Vector<String>> chains;
+	private HashMap<Vector<String>, DenseBag> chains;
 	private HashMap<String, Integer> totalOccurances;
 	private Vector<String> starterWords;
 	private Vector<String> endWords;
 	
+	private static final int ORDER = 2;
 	
 	public Markov(){
-		this.chains = new HashMap<String, Vector<String>>();
+		this.chains = new HashMap<Vector<String>, DenseBag>();
 		this.totalOccurances = new HashMap<String, Integer>();
 		this.starterWords = new Vector<String>();
 		this.endWords = new Vector<String>();
 	}
 	
+	private void put(Vector<String> prefix, String suffix){
+		if(prefix.get(0))
+	}
 }
