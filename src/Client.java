0e4937c5314268d977df0b927d48303a8e4b821e
Mike Melchione <michaelmelchione@gmail.com>
2016-01-05 23:04:04 -0500

    Added some tests in main method

--- src/Client.java
+++ src/Client.java
@@ -1,4 +1,11 @@
+import markov.Markov;
 
 public class Client {
-
+	
+	public static void main(String args[]){
+		Markov markov = new Markov();
+		markov.addSentence("This is a nice sentence. This is another sentence. This is another sentence.");
+		System.out.println(markov.toString());
+		System.out.println(markov.generate());
+	}
 }
